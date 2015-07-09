8af496f25593c1e80b39d38fbf248b7b7b6fc5ad
georgiadavis <gyang925@gmail.com>
2015-07-08 23:40:51 -0700

    add solution

---
+++ Solution/solution.py
@@ -0,0 +1,17 @@
+four_letter_countries= ['Chad','Cuba', 'Greenland', 'Iraq', 'Mali', 'Oman', 'India']
+four_letter_countries.append('Fiji')
+four_letter_countries
+four_letter_countries.insert( 3, 'Iran')
+four_letter_countries
+four_letter_countries.remove('India')
+four_letter_countries
+four_letter_countries.reverse()
+four_letter_countries
+four_letter_countries[5]="Togo"
+four_letter_countries
+four_letter_countries.append('Laos')
+four_letter_countries
+four_letter_countries.reverse()
+four_letter_countries
+four_letter_countries.insert(0,'Peru')
+four_letter_countries
